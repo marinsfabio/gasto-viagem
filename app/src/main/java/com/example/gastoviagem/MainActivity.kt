1804036640f6a8b3c0c7551903567358b9462ef8
marinsfabio <fabiomarinspro@hotmail.com>
2023-09-05 23:38:55 -0300

    funcao

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.example.gastoviagem
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView (binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+        val totalValue = (distance * price) / autonomy
+       // val totalValueStr = "R$ ${"%.2f".format(totalValue)}"
+
+        binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+        //Toast.makeText(this, totalValueStr, Toast.LENGTH_SHORT).show()
     }
 }
